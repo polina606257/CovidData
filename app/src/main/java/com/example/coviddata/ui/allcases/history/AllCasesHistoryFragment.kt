7781f89d6979fbaffebe3be61f976c4ed57c1c20
polina606257 <polina.shcherbinina@gmail.com>
2020-11-17 14:31:54 +0600

    RecycleView without data from local database

--- app/src/main/java/com/example/coviddata/ui/allcases/history/AllCasesHistoryFragment.kt
+++ app/src/main/java/com/example/coviddata/ui/allcases/history/AllCasesHistoryFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.covidappapi.model.AllCases
 import com.example.coviddata.R
+import com.example.coviddata.ui.countriescases.ListAdapter
+import kotlinx.android.synthetic.main.fragment_all_cases_history.*
+import kotlinx.android.synthetic.main.fragment_cases_countries.*
+import java.time.LocalDateTime
 
 class AllCasesHistoryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+            : View? {
+        val root = inflater.inflate(R.layout.fragment_all_cases_history, container, false)
+
+        return root
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_all_cases_history, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val list: List<AllCases> = listOf(AllCases(200, 30, 15, LocalDateTime.now().toString()),
+        AllCases(300, 30, 40, LocalDateTime.now().toString()))
+
+                historyCasesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        historyCasesRecyclerView.adapter = ListAdapter(list)
     }
 }
--- app/src/main/java/com/example/coviddata/ui/allcases/history/HistoryCasesAdapter.kt
+++ app/src/main/java/com/example/coviddata/ui/allcases/history/HistoryCasesAdapter.kt
@@ -1,2 +1,38 @@
 package com.example.coviddata.ui.allcases.history
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.covidappapi.model.AllCases
+import com.example.coviddata.R
+import kotlinx.android.synthetic.main.history_cases_item.view.*
+
+class ListAdapter(private val list: List<AllCases>)
+    : RecyclerView.Adapter<ListAdapter.HistoryCasesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryCasesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.history_cases_item, parent, false)
+        val holder = HistoryCasesViewHolder(view)
+        return holder
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: HistoryCasesViewHolder, position: Int) {
+        val historyCases = list[position]
+        holder.bind(historyCases)
+    }
+
+    inner class HistoryCasesViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        var historyCases: AllCases? = null
+
+        fun bind(item: AllCases) {
+            this.historyCases = item
+            itemView.date_time_history.text = "Date and time:   ${item.datetime}"
+            itemView.all_cases_per_date_time.text = "Cases:  ${item.cases}"
+            itemView.all_deaths_per_date_time.text = "Deaths:  ${item.deaths}"
+            itemView.all_recovered_per_date_time.text = "Recovered:  ${item.recovered}"
+        }
+    }
+}
