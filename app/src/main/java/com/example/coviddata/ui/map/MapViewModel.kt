afd6899bf577545eba439cd5edc6b001b3a03574
polina606257 <polina.shcherbinina@gmail.com>
2020-11-30 12:58:36 +0600

    finished - map pin color (moved class ColorConverter to different file)

--- app/src/main/java/com/example/coviddata/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/coviddata/ui/map/MapViewModel.kt
@@ -17,33 +17,6 @@ class MapViewModel : ViewModel() {
 
     fun getGroupNumber(country: CountryData) = converter.getGroupNumber(country)
 }
-class ColorGroupConverter(val listCountries: List<CountryData>){
-    fun getGroupNumber(country: CountryData): Int {
-        return when (country.casesPerOneMillion) {
-            in getCasesRelatedForGroup(0.0, 0.1) ->
-                1
-            in getCasesRelatedForGroup(0.1, 0.3) ->
-                2
-            in getCasesRelatedForGroup(0.3, 0.6) ->
-                3
-            in getCasesRelatedForGroup(0.6, 0.8) ->
-                4
-            else ->
-                5
-        }
-    }
-
-    private fun getCasesRelatedForGroup(percentForMin: Double, percentForMax: Double) : ClosedFloatingPointRange<Double> {
-        val countryMinCasesPerOneMillion = listCountries.minByOrNull { it.casesPerOneMillion }
-        val countryMaxCasesPerOneMillion = listCountries.maxByOrNull { it.casesPerOneMillion }
-        val minNumCasesPerGroup = (countryMaxCasesPerOneMillion!!.cases - countryMinCasesPerOneMillion!!
-                .cases)* percentForMin
-        val maxNumCasesPerGroup = (countryMaxCasesPerOneMillion.cases - countryMinCasesPerOneMillion
-                .cases) * percentForMax
-        return minNumCasesPerGroup..(maxNumCasesPerGroup)
-    }
-}
-
 
 
 
